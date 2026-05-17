fdef2f5ac060e35251397adc72730e1c1094c146
Charles Bato <charlesbato28@gmail.com>
2026-05-18 00:30:44 +0800

    fill empty pages and clean up folders

---
+++ app/admin/login/page.tsx
@@ -0,0 +1,3 @@
+export default function Placeholder() {
+  return <div className="p-20 font-serif">Under Construction</div>;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,9 @@
-import "@/app/globals.css";
+import "./globals.css";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {    
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
