7b06f76f4d98224d4fbec66d155d6614a41e7968
charles bato <charlesbato28@gmail.com>
2026-09-10 12:54:39 +0800

    Fix routing for Vercel deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 // This file forces Next.js to load your guest page at the root URL "/"
-import GuestPage from "./(guest)/page";
+import GuestPage from "./guest/page";
 
 export default function Home() {
   return <GuestPage />;
