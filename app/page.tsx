72300c669fe36f4848934340e99c33401da74667
charles bato <charlesbato28@gmail.com>
2026-09-10 13:24:58 +0800

    Restore full wedding website

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,4 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '50px', textAlign: 'center' }}>
-      <h1>Pao & Dei</h1>
-      <p>Wedding Website</p>
-      <p style={{ color: 'green' }}>✓ If you see this, routing works!</p>
-    </div>
-  );
-}
+  import { redirect } from 'next/navigation';
+  export default function GuestPage() {
+    redirect('/');
+  }
