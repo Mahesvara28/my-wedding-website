a1826b4756d80021a94479cfad1f3cc8b4aa9360
charles bato <charlesbato28@gmail.com>
2026-09-15 19:21:20 +0800

    Fix Principal Sponsors order: move Marlyn & Ar. Dominador after Lorna & Claro

--- components/sections/Sponsors.tsx
+++ components/sections/Sponsors.tsx
@@ -38,9 +38,10 @@ export default function Sponsors() {
               ["Tristana Umali", "Atty. Al Matthew Umali"],
               ["Rosario Carla Dionisio", "Atty. Lorenze Angelo Dionisio"],
               ["Lorna M. Funtanilla", "Claro R. Funtanilla"],
-              ["Kathryn Joyce Cabochan", "Charlie Rodriguez"],
               ["Marlyn M. Dayag", "Ar. Dominador Macatuggal"],
+              ["Kathryn Joyce Cabochan", "Charlie Rodriguez"],
               ["Cleo Alvero", "Edgar Tordesillas"], 
+              ["Marlyn M. Dayag", "Ar. Dominador Macatuggal"],
               ["Maria Lourdes Violeta Gaerlan", "German Maravilla"],
               ["Mary Ann Mendoza", "Alberto Gochangco"],
             ].map((couple, i) => (
